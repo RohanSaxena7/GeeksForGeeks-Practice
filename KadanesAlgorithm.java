8f2ae6eaff6ca8b6cd50d528a9eeb876f33b9f54
Rohan Saxena <117259135+RohanSaxena7@users.noreply.github.com>
2024-03-13 23:08:14 +0530

    Create KadanesAlgorithm.java

---
+++ KadanesAlgorithm.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+import java.io.*;
+
+import java.util.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    //size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling maxSubarraySum() function
+		    System.out.println(obj.maxSubarraySum(arr, n));
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+class Solution{
+
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        long current = 0;
+        long max = arr[0];
+        
+        for (int i= 0; i<n; i++){
+            current = current + arr[i];
+            max = Math.max(current, max);
+            if (current < 0){
+                current = 0;
+            }
+        }
+        return max;
+        
+    }
+    
+}
+
